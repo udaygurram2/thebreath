a280df6cfe6082ca923276d2be79907408fbe59a
udaygurram2 <udaygurram2410@gmail.com>
2025-05-19 22:39:05 +0530

    Update forms to handle email submissions

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -28,21 +28,6 @@ const ContactPage: React.FC = () => {
           </div>
         </div>
       </section>
-      
-      {/* Map Section */}
-      <section className="h-[400px] relative">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.3059353029!2d-74.25986548248684!3d40.697149419326095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sca!4v1652813390700!5m2!1sen!2sca"
-          width="100%"
-          height="100%"
-          style={{ border: 0 }}
-          allowFullScreen={false}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Breathe Location"
-        ></iframe>
-      </section>
-    </PageWrapper>
   );
 };
 
