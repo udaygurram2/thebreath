c7a1f370213e398a446f3652dcabd67c66a87800
udaygurram2 <udaygurram2410@gmail.com>
2025-05-31 12:14:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import MainLayout from './layout/MainLayout';
